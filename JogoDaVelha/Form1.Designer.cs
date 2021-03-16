d3614053b677ab296ef7c030eae4ad96028a5cdb
gutemberg3078 <gsilva@iblueconsulting.com.br>
2021-03-16 10:50:40 -0300

    Jogo da Velha

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(80, 74);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(50, 50);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+           
+            // 
+            // b12
+            // 
+            this.b12.Location = new System.Drawing.Point(80, 130);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(50, 50);
+            this.b12.TabIndex = 3;
+            this.b12.UseVisualStyleBackColor = true;
+            // 
+            // b13
+            // 
+            this.b13.Location = new System.Drawing.Point(80, 186);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(50, 50);
+            this.b13.TabIndex = 5;
+            this.b13.UseVisualStyleBackColor = true;
+            // 
+            // b21
+            // 
+            this.b21.Location = new System.Drawing.Point(136, 74);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(50, 50);
+            this.b21.TabIndex = 1;
+            this.b21.UseVisualStyleBackColor = true;
+            // 
+            // b22
+            // 
+            this.b22.Location = new System.Drawing.Point(136, 130);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(50, 50);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            // 
+            // b23
+            // 
+            this.b23.Location = new System.Drawing.Point(136, 186);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(50, 50);
+            this.b23.TabIndex = 7;
+            this.b23.UseVisualStyleBackColor = true;
+            // 
+            // b31
+            // 
+            this.b31.Location = new System.Drawing.Point(192, 74);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(50, 50);
+            this.b31.TabIndex = 2;
+            this.b31.UseVisualStyleBackColor = true;
+            // 
+            // b32
+            // 
+            this.b32.Location = new System.Drawing.Point(192, 130);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(50, 50);
+            this.b32.TabIndex = 6;
+            this.b32.UseVisualStyleBackColor = true;
+            // 
+            // b33
+            // 
+            this.b33.Location = new System.Drawing.Point(192, 186);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(50, 50);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(55, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "X,  é a sua vez.";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 348);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        bool xis = true;  // se for true é a vez do jogador X, caso nao sera o jogador Zero.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // metodo criado para tratar o evendo de click
+            b11.Click += new EventHandler(BClick);
+            b12.Click += new EventHandler(BClick);
+            b13.Click += new EventHandler(BClick);
+            b21.Click += new EventHandler(BClick);
+            b22.Click += new EventHandler(BClick);
+            b23.Click += new EventHandler(BClick);
+            b31.Click += new EventHandler(BClick);
+            b32.Click += new EventHandler(BClick);
+            b33.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)// percorre a lista de todos os controles 
+            {
+                if (item is Button) // se o item for um botao faça  
+                {
+                    item.TabStop = false; // desliga a propriedade TabStop, para que o jogador nao seja influenciado a apertar 
+                                          // o proximo botão 
+                }
+
+            }
+
+
+        }
+
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.xis ? "X" : "O";// se o texto X for true ou vise e versa 
+            ((Button)sender).Enabled = false;//
+            verificarGanhador();
+
+
+            xis = !xis; // inverte o X pra falar quem é a vez do proximo jogador 
+
+
+            button1.Text = string.Format("{0}, é a sua vez", this.xis ? "X" : "O"); // diz qual a VEZ do jogador referente ao simbolo escolhido
+
+        }
+
+        private void verificarGanhador()
+        {
+
+
+
+            if (b11.Text != String.Empty && b11.Text == b12.Text && b12.Text == b13.Text || //linha  1 // compara os botões das linhas se os 3 sao iguais ou diferentes de vazio 
+               b21.Text != String.Empty && b21.Text == b22.Text && b22.Text == b23.Text || //linha 2
+               b31.Text != String.Empty && b31.Text == b32.Text && b32.Text == b33.Text ||//linha  3
+
+               b11.Text != String.Empty && b11.Text == b21.Text && b21.Text == b31.Text || //coluna 1// o mesmo teste para as 3 colunas 
+               b12.Text != String.Empty && b12.Text == b22.Text && b22.Text == b32.Text ||//coluna 2
+               b13.Text != String.Empty && b13.Text == b23.Text && b23.Text == b33.Text ||//coluna 3
+
+
+               b11.Text != String.Empty && b11.Text == b22.Text && b22.Text == b33.Text || // diagonal esquerta pra direita // e tambem as diagonais 
+               b13.Text != String.Empty && b13.Text == b22.Text && b22.Text == b31.Text)// diagonal direita pra esquerda      
+
+            {
+                MessageBox.Show(string.Format("O ganhador é o [{0}]", xis ? "X" : "O"), " Voce é o Vencedor", // da uma messagem referindo ao jogador que ganhou
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+            }
+            else
+            {
+                verificarEmpate(); // função que retorna empate 
+            }
+
+
+        }
+        private void verificarEmpate()
+        {
+
+            bool todoDesabilitados = true;
+            bool x = false;
+
+            foreach (Control item in this.Controls)// percorre todo os controles 
+            {
+                if (item is Button && item.Enabled) // se o item for abilitado e for um botao 
+                {
+                    todoDesabilitados = false; // retorne falso 
+                    break;
+                   
+
+                }
+                
+
+            }
+
+            if(x){
+                MessageBox.Show(String.Format("deu empate"), "OPs !!!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Reiniciar(); // se todos forem abilitados reinicia todos os botoes     
+            }
+
+
+        }
+
+
+        private void Reiniciar()
+        {
+            foreach (Control item in this.Controls)//percorre a coleções de botões
+            {
+                if (item is Button)
+                    item.Enabled = true; //se o item for um botão abilite 
+                item.Text = String.Empty; // e o texto dele vira uma string vazia 
+            }
+        }
+
+
+    }
+}
+
+
